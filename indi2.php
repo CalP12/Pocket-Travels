660a0e8d41b8d06232ce4f4b27d596e4f086d465
Calvin Pinto <calvinpinto5@gmail.com>
2021-01-12 17:19:36 +0530

    Update indi2.php

--- indi2.php
+++ indi2.php
@@ -111,10 +111,10 @@ $pdf->Output(F,'tickets/ticket.pdf');
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = "smtp.gmail.com";                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = "4nm18cs040@nmamit.in";                     // SMTP username
-    $mail->Password   = "shawns541";                               // SMTP password
+    $mail->Username   = "";                     // SMTP username
+    $mail->Password   = "1";                               // SMTP password
     $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->Port       = ;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
     $mail->setFrom("4nm18cs040@nmamit.in","Pocket Travels");
